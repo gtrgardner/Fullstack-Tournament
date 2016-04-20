78d333f11e6997efa51851a52e0b7d46d0105e2d
robert gardner <gtrgardner@gmail.com>
2016-04-20 18:52:50 -0500

    modified schema

--- tournament.sql
+++ tournament.sql
@@ -6,4 +6,26 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE DATABASE tournament;
 
+CREATE TABLE player_register(
+	id serial,
+	name text,
+	
+	primary key(id)
+	);
+
+
+CREATE TABLE standings(
+	id serial null references players(id),
+	name text,
+	matches_played int,
+	matches_won int
+);
+
+CREATE TABLE pairings(
+	id1 int,
+	name1 text,
+	id2 int,
+	name2 text
+);
