b86bf7fd296ad32e5da0fef166101fc99d7b8041
robert gardner <gtrgardner@gmail.com>
2016-04-23 15:21:09 -0500

    modify schema

--- tournament.sql
+++ tournament.sql
@@ -5,34 +5,33 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
-DROP DATABASE IF EXISTS tournament;
+--DROP DATABASE IF EXISTS tournament;
 
 CREATE DATABASE tournament;
 
 \c tournament;
-DROP TABLE IF EXISTS player_register;
-DROP TABLE IF EXISTS standings;
-DROP TABLE IF EXISTS pairings;
+--DROP TABLE IF EXISTS Players;
+--DROP TABLE IF EXISTS standings;
+--DROP TABLE IF EXISTS Matches;
 
-CREATE TABLE player_register(
-	id serial,
-	name text,
-	
-	primary key(id)
+CREATE TABLE Players(
+	id serial PRIMARY KEY,
+	name text
 	);
 
 
 CREATE TABLE standings(
-	id int null references player_register(id),
+	id int null references Players(id),
 	name text,
 	matches_played int,
-	matches_won int
-);
+	matches_won int,
+	matches_lost int
+	);
 
-CREATE TABLE pairings(
-	match_id serial primary key,
-	id1 int null references player_register(id),
+CREATE TABLE Matches(
+	match_id serial PRIMARY KEY,
+	id1 int null references Players(id),
 	name1 text,
-	id2 int null references player_register(id),
+	id2 int null references Players(id),
 	name2 text
-);
+	);
